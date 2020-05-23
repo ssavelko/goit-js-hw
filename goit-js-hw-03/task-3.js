1be6fbe0447885a40103835de506f52da2e0d543
ssavelko <savelkos@gmail.com>
2020-05-23 22:11:49 +0300

    hw3

---
+++ goit-js-hw-03/task-3.js
@@ -0,0 +1,42 @@
+const findBestEmployee = function(employees) {
+    let max = 0;
+    let bestEmployee;
+
+    for ( const employee in employees){
+        const numOfTask = employees[employee];
+        if (numOfTask > max) {
+            max = numOfTask;
+            bestEmployee = employee
+        }
+    }
+    return bestEmployee;
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(
+    findBestEmployee({
+      ann: 29,
+      david: 35,
+      helen: 1,
+      lorence: 99,
+    }),
+  ); // lorence
+  
+  console.log(
+    findBestEmployee({
+      poly: 12,
+      mango: 17,
+      ajax: 4,
+    }),
+  ); // mango
+  
+  console.log(
+    findBestEmployee({
+      lux: 147,
+      david: 21,
+      kiwi: 19,
+      chelsy: 38,
+    }),
+  ); // lux
